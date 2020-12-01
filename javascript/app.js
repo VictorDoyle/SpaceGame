d7ffe22f643ee44bfde8a2c185f88e0d6d054b20
victordoyle <victor-doyle@outlook.com>
2020-12-01 18:23:12 -0500

    Evil Ai image tweaks + animations

--- javascript/app.js
+++ javascript/app.js
@@ -1,9 +1,16 @@
 console.log("Insanity Check?")
 
+/* LATER GAME SEQUENCES AND SCREENS HAVE TO BEGIN AT HIDDEN */
+$(".earlyQuitter").hide();
+$(".partTwo").hide();
+$(".partThree").hide();
+
 /* ANIMATION TO FADE OUT WHEN PLAY BUTTON CLICKED*/
 
 $(".playGame").on("click", function fadeOutofScreen(){
-    $("div .partOne").fadeOut(1000)
+    $("div .partOne").fadeOut(1000);
+    $("div .partTwo").fadeIn(1000);
+    
 }
 )
 
@@ -11,6 +18,13 @@ $(".playGame").on("click", function fadeOutofScreen(){
 /* ANIMATION TO FADE OUT WHEN NO DONT PLAY BUTTON CLICKED*/
 
 $(".abortGame").on("click", function fadeOutofScreen(){
-    $("div .partOne").fadeOut(1000)
+    $("div .partOne").fadeOut(500);               // FIXME: fix fadeout animation
+   $(".earlyQuitter").show();
+   $(".evilHal").fadeIn(1000);      //FIXME: Animation wont work
+
 }
 )
+
+
+/* GAME RULES/BASICS JAVASCRIPT */
+
