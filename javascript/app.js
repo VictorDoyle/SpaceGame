a9b2c66c6089e1e813aa5a936d06f301ec030173
victordoyle <victor-doyle@outlook.com>
2020-12-01 17:23:51 -0500

    safety push

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,12 @@
 console.log("Insanity Check?")
+
+/* ANIMATION TO SLIDE ELEMENTS LEFT OFF PAGE WHEN BUTTON CLICKED*/
+
+function slideLeft() {
+    document.getElementById("introduction");
+  }
+/* ANIMATION TO SLIDE ELEMENTS RIGHT OFF PAGE WHEN USER CHOOSES DONT PLAY GAME*/
+
+  function slideLeft() {
+    document.getElementById("introduction");
+  }
