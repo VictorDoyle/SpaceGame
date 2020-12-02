cecc75ed93062a6008ec151424b07186e096709e
victordoyle <victor-doyle@outlook.com>
2020-12-01 21:06:00 -0500

    added Player Class w/ Minor Basics

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,4 @@
-console.log("Insanity Check?")
+
 
 /* LATER GAME SEQUENCES AND SCREENS HAVE TO BEGIN AT HIDDEN */
 $(".earlyQuitter").hide();
@@ -20,9 +20,9 @@ $(".playGame").on("click", function fadeOutofScreen(){
 /* ANIMATION TO FADE OUT WHEN NO DONT PLAY BUTTON CLICKED*/
 
 $(".abortGame").on("click", function fadeOutofScreen(){
-    $("div .partOne").fadeOut(500);               // FIXME: fix fadeout animation
+    $("div.partOne").fadeOut(500);               // FIXME: fix fadeout animation
    $(".earlyQuitter").show();
-   $("div .evilHal img").fadeIn(1000);      //FIXME: Animation wont work
+   $("div.evilHal img").fadeIn(1000);      //FIXME: Animation wont work
 
 }
 )
@@ -31,3 +31,45 @@ $(".abortGame").on("click", function fadeOutofScreen(){
 /* GAME RULES/BASICS JAVASCRIPT */
 
 /* Input + Button for character name submission */
+
+const $submitButton =  $("button.submitButton") 
+
+/* Initialize Name Submission with Button Click - Store value and return it in Player class */
+$submitButton.on("click", function logPlayerName(){
+    $("p.dialogueBox").fadeOut(1000);
+    let player = document.getElementById('astronautName');
+    let playerName = player.value;
+   console.log(playerName);
+
+}
+)
+
+
+
+/* PLAYER BASICS */
+
+class Player {
+    constructor(givenName) {
+      // default props
+      this.hungerLevel = 0;
+      this.sleepLevel = 0;
+      this.boredLevel = 0;
+      // assigned props
+      this.name = givenName;
+      
+    }
+  ​
+    getHungry() {
+      console.log("I'm feeling hungry, maybe it's time for a snack?");
+    }
+    
+    getBored() {
+    console.log("I am feeling an existential crisis coming soon. I need something to do...");
+  }
+    
+    getSleepy() {
+    console.log("Is that a purple unicorn? Urgh, I think I need a nap. I'm starting to see things...");
+  }
+  }
+
+  const firstAstronaut = new Player(""); /* Have to link .value from input to become new Player (input.value)*/
